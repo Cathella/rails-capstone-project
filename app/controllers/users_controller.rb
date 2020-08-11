a80ee263d9c3d622d485c54d989c392fce11503d
Cathella <ncathie214@gmail.com>
2020-08-11 20:06:27 +0300

    create users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+  def index
+
+  end
+
+  def profile
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/profile'
+  # get '/profile', to: 'users#profile'
+
+  # get '/newgroup', to: 'groups#new'
+  # get '/newtransaction', to: 'transactions#new'
+  # get '/etransactions', to: 'transactions#etransaction'
+  # get '/memberstransactions', to: 'transactions#members_transactions'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
+
+  # root 'users#profile'
+
+  # devise_scope :user do
+  #   get 'signin', to: 'devise/sessions#new'
+  #   get 'edit_profile', to: 'devise/registrations#edit'
+  #   get 'signup', to: 'devise/registrations#new'
+  #
+  #   authenticated :user do
+  #     root 'users#profile'
+  #   end
+  #
+  #   unauthenticated do
+  #     root 'users#index'
+  #   end
+  # end
 end
