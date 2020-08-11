3b5861849b4ae277295b61079f56ecf16102e549
Cathella <ncathie214@gmail.com>
2020-08-11 20:16:20 +0300

    add route paths for the views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # get 'users/index'
-  # get 'users/profile'
-  # get '/profile', to: 'users#profile'
+  get 'users/index'
+  get 'users/profile'
+  get '/profile', to: 'users#profile'
 
   # get '/newgroup', to: 'groups#new'
   # get '/newtransaction', to: 'transactions#new'
@@ -12,17 +12,17 @@ Rails.application.routes.draw do
 
   # root 'users#profile'
 
-  # devise_scope :user do
-  #   get 'signin', to: 'devise/sessions#new'
-  #   get 'edit_profile', to: 'devise/registrations#edit'
-  #   get 'signup', to: 'devise/registrations#new'
-  #
-  #   authenticated :user do
-  #     root 'users#profile'
-  #   end
-  #
-  #   unauthenticated do
-  #     root 'users#index'
-  #   end
-  # end
+  devise_scope :user do
+    get 'signin', to: 'devise/sessions#new'
+    get 'edit_profile', to: 'devise/registrations#edit'
+    get 'signup', to: 'devise/registrations#new'
+
+    authenticated :user do
+      root 'users#profile'
+    end
+
+    unauthenticated do
+      root 'users#index'
+    end
+  end
 end
