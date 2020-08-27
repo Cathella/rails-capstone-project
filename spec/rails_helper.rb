77b4ef0c250e7811917e9f2c7d96fcb87672407c
Cathella <ncathie214@gmail.com>
2020-08-27 10:17:26 +0300

    add tests for the models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,12 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -19,13 +14,4 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-
-  Shoulda::Matchers.configure do |c|
-    c.integrate do |with|
-      # Choose a test framework:
-      with.test_framework :rspec
-      with.library :rails
-    end
-  end
-
 end
